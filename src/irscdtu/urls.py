4b79c9c559e3dabc6bc575dbee08e391348993cc
gul <gulasnani7@gmail.com>
2018-03-18 18:55:23 +0530

    Tags and urls aligned with django

--- src/irscdtu/urls.py
+++ src/irscdtu/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^contact/$', views.contact, name='contact'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
