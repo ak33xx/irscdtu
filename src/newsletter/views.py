d19a52e8332da87fa4f57744ca17b927c519374d
gul <gulasnani7@gmail.com>
2018-04-03 22:05:07 +0530

    Commit 2 on auto mailer

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -5,10 +5,21 @@ from django.http import HttpResponse, HttpResponseRedirect
 from . import models
 
 # Create your views here.
-def send_email(request,subject,message):
-    q = models.MailTo.objects.all()
-    for p in q:
-        try:
-            send_mail(subject, message, from_email, p.email_id)
-        except BadHeaderError:
-            return HttpResponse('Invalid header found.')
+def send_email(request):
+    form = forms.MailForm()
+    if request.method == 'POST':
+        form = forms.MailForm(request.POST)
+        if form.is_valid():
+            q = models.MailTo.objects.all()
+            for p in q:
+                try:
+                    send_mail(
+                        form.cleaned_data['subject'],
+                        form.cleaned_data['message'],
+                        None,
+                        p.email_id
+                    )
+                except BadHeaderError:
+                    return HttpResponse('Invalid header found.')
+            return HttpResponseRedirect(reverse('mailform'))
+    return render(request, 'mail_form.html', {'form': form})
