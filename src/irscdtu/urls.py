18bae25d890fba8974c08c1bbb06e6d9d8681f2b
gul <gulasnani7@gmail.com>
2018-04-12 00:06:24 +0530

    useless commit

--- src/irscdtu/urls.py
+++ src/irscdtu/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import url, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -28,4 +30,4 @@ urlpatterns = [
     url(r'^contact/$', views.contact, name='contact'),
 ]
 
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
